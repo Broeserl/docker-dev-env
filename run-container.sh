379df04279d2df286f0a7df5d209a5d3f92581d4
Johannes Juraske <johannes.juraske@msys-gmbh.de>
2021-05-03 07:48:20 +0200

    Adjusted run-container.sh

--- run-container.sh
+++ run-container.sh
@@ -9,4 +9,4 @@ fi
 
 docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name $name $name 
 
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[127.0.0.1]:2222"
